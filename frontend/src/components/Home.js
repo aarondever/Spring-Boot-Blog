134e7242b8fe0ea050958230b0962ee3da8549cd
Aaron <troublemaker.5354@gmail>
2023-08-19 21:13:12 +0800

    Add search icon to search bar

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -96,7 +96,14 @@ function Home() {
                         <div className="col-10">
                             <form onSubmit={handleSearchSubmit}>
                                 <div className="input-group rounded">
-                                    <input type="search" className="form-control" placeholder="search title or content" name="search" />
+                                    <span class="input-group-text">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search"
+                                            viewBox="0 0 16 16">
+                                            <path
+                                                d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                                        </svg>
+                                    </span>
+                                    <input type="search" className="form-control" placeholder="Search title or content" name="search" />
                                     <button type="submit" className="btn btn-outline-primary">Search</button>
                                 </div>
                             </form>
